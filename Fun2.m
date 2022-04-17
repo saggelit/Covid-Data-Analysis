47e6b4ca04141d6180725ca81e75e21835c940ab
saggelit <60180432+saggelit@users.noreply.github.com>
2022-04-17 04:40:27 +0300

    Update and rename AggelitshExe2Fun2.m to Fun2.m

--- Fun2.m
+++ Fun2.m
@@ -1,10 +1,7 @@
 function [hC,pC,hD,pD]=fun2_2(country,country_confirmed,country_deaths)
-%fprintf('%s:\n',country)
 pdC=fitdist(country_confirmed,'Kernel','Kernel','epanechnikov');
 [hC,pC]=chi2gof(country_confirmed,'CDF',pdC);
-%fprintf('Kernel-Cases: h=%f,p=%f\n',hC,pC)
 
 pdD=fitdist(country_deaths,'Kernel'); %pd:propability distribution
 [hD,pD]=chi2gof(country_deaths,'CDF',pdD);
-%fprintf('Kernel-Deaths: h=%f,p=%f\n\n',hD,pD)
 end 
